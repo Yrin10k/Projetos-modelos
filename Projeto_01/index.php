318a279f53344403c35a037c1ec84cf8401122a7
Ayron Nascimento <nayrton383@gmail.com>
2023-07-04 23:21:20 -0300

    Projeto Danki Code - 01
    projeto 01 danki code, curso
    dev web 1.0;
    
    completo:
    html , css
    responsividade total;
    
    em andamento:
    javascript;
    Jquery;
    
    faltando :
    Banco de dados(MySql);
    paginas de admin;
    controle de clientes;
    entre outros...;

---
+++ Projeto_01/index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head> 
+<link rel="stylesheet" href="estilo/font-awesome.min.css">
+ 
+    <link rel="stylesheet" href="estilo/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Descrição do meu website">
+    <meta name="Keywords" content="Palavras-chaves,do,meu,site">
+
+    <title>Projeto 01</title>
+</head>
+<body>
+    
+   <header>
+    <div class="center">
+        <div class="logo left">Logomarca</div><!--logo-->
+        <nav class="desktop right">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+        <nav class="mobile right">
+            <div class="botao-menu-mobile"><i class="fa fa-bars" aria-hidden="true"></i>
+</div>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+        <div class="clear"></div><!--clear-->
+    </div><!--center-->
+   </header>
+ 
+ <section class="banner-principal">
+ <div class="overlay"></div><!--overlay-->    
+ <div class="center">
+         <form action="">
+           <h2>Qual o seu melhor e-mail?</h2>
+           <input type="email" name="email" id="" required>
+           <input type="submit" value="Cadastrar!">
+         </form>
+     </div><!--center-->
+ </section><!--banner-principal-->
+
+   <section class="descricao-autor">
+   <div class="center">
+   <div class="w50 left"> 
+     <h2>Ayron Nasc. Silva</h2>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum saepe in praesentium, ipsa asperiores eius dolore amet quae nihil id possimus, quo pariatur, incidunt vel omnis itaque ad aliquid. Sunt! Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam qui perspiciatis quia quaerat alias cumque quae illum. Velit necessitatibus natus quaerat enim similique libero vitae ex temporibus? Consequatur, laboriosam impedit!</p>
+     
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum saepe in praesentium, ipsa asperiores eius dolore amet quae nihil id possimus, quo pariatur, incidunt vel omnis itaque ad aliquid. Sunt! Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam qui perspiciatis quia quaerat alias cumque quae illum. Velit necessitatibus natus quaerat enim similique libero vitae ex temporibus? Consequatur, laboriosam impedit!</p>
+    
+         </div><!--W50-->
+         
+         <div class="w50 left">
+           <img src="imagens/fotoayron.png" alt="" class="right">
+         </div><!--W50-->
+         <div class="clear"></div>
+     </div><!--center-->
+   </section><!--descricao-autor-->
+   
+   <section class="especialidades">
+
+   <div class="center">
+   <h2 class="title">Minhas Especialidades</h2>
+<div class="w33 left box-especialidade">
+<h3><i class="fa fa-css3" aria-hidden="true"></i>
+</h3>
+<h4>CSS3</h4>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio nisi id cum laborum repudiandae amet error, ad sapiente mollitia aliquam obcaecati perferendis deserunt! Magni voluptatibus illo explicabo quo unde consectetur!
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur est hic consequatur magni. Perspiciatis, quo! Earum recusandae consequuntur esse cum, ex, incidunt adipisci neque quae eaque obcaecati beatae eum qui.
+</p>
+</div><!--box-especialidade-->
+
+<div class="w33 left box-especialidade">
+<h3><i class="fa fa-html5" aria-hidden="true"></i>
+</h3>
+<h4>HTML5</h4>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio nisi id cum laborum repudiandae amet error, ad sapiente mollitia aliquam obcaecati perferendis deserunt! Magni voluptatibus illo explicabo quo unde consectetur!
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur est hic consequatur magni. Perspiciatis, quo! Earum recusandae consequuntur esse cum, ex, incidunt adipisci neque quae eaque obcaecati beatae eum qui.
+</p>
+</div><!--box-especialidade-->
+
+<div class=" w33 left box-especialidade">
+<h3><i class="fa fa-connectdevelop" aria-hidden="true"></i>
+</h3>
+<h4>JavaScript</h4>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio nisi id cum laborum repudiandae amet error, ad sapiente mollitia aliquam obcaecati perferendis deserunt! Magni voluptatibus illo explicabo quo unde consectetur!
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur est hic consequatur magni. Perspiciatis, quo! Earum recusandae consequuntur esse cum, ex, incidunt adipisci neque quae eaque obcaecati beatae eum qui.
+</p>
+</div><!--box-especialidade-->
+<div class="clear"></div>
+   </div><!--center-->
+</section><!--especialidades-->
+
+<section class="extras">
+<div class="center">
+    <div class="w50 left depoimentos-container">
+        <h2 class="title">Depoimentos dos nossos clientes</h2>
+
+        <div class="depoimento-single">
+            <p class="depoimento-descricao">"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis excepturi animi et consectetur, tempora reiciendis soluta sint sed optio nemo a necessitatibus consequuntur vero ipsum magni odit deleniti eos voluptatem! Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto alias volup"</p>
+            <p class="nome-autor">Loren Ipsum</p>
+        </div><!--depoimento-single-->
+        
+        <div class="depoimento-single">
+            <p class="depoimento-descricao">"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis excepturi animi et consectetur, tempora reiciendis soluta sint sed optio nemo a necessitatibus consequuntur vero ipsum magni odit deleniti eos voluptatem! Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto alias volup"</p>
+            <p class="nome-autor">Loren Ipsum</p>
+        </div><!--depoimento-single-->
+        
+        <div class="depoimento-single">
+            <p class="depoimento-descricao">"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis excepturi animi et consectetur, tempora reiciendis soluta sint sed optio nemo a necessitatibus consequuntur vero ipsum magni odit deleniti eos voluptatem! Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto alias volup"</p>
+            <p class="nome-autor">Loren Ipsum</p>
+        </div><!--depoimento-single-->
+
+    </div><!--w50-->
+
+    <div class="w50 left servicos-container">
+        <h2 class="title">Serviços</h2>
+        <div class="servicos">
+            <ul>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet laudantium eveniet officiis, debitis maxime quidem minus, doloremque itaque sequi consequuntur, quos tempora dolorum quasi illo ea. Aut praesentium autem ratione?</li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet laudantium eveniet officiis, debitis maxime quidem minus, doloremque itaque sequi consequuntur, quos tempora dolorum quasi illo ea. Aut praesentium autem ratione?</li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet laudantium eveniet officiis, debitis maxime quidem minus, doloremque itaque sequi consequuntur, quos tempora dolorum quasi illo ea. Aut praesentium autem ratione?</li>
+            </ul>
+        </div><!--servicos-->
+    </div><!--w50-->
+    <div class="clear"></div>
+</div><!--center-->
+</section><!--extras-->
+
+<footer>
+ <div class="center">
+   <p>Todos os direitos reservados</p>
+ </div><!--center-->
+</footer>
+<script src="js/jquery.js"></script>
+<script>
+/* $(function(){
+     var mensagem = "Olá Mundo"; // Declaração de Variavéis 
+     console.log(mensagem);
+      alert(mensagem);
+     })  //nome da função no Jquery é $; */
+</script>
+</body>
+</html>
