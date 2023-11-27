76fb8fb23b989cf5a57d826841a3d48da7df06eb
Ayron Nascimento <128555395+Yrin10k@users.noreply.github.com>
2023-11-27 19:33:51 -0300

    Funcoes CRUD basicas beta Acabada
    select falta ideias para implementacao, preciso da sua ajuda para me dizer o que gostaria no select

--- phpfuncs/functions.php
+++ phpfuncs/functions.php
@@ -1,8 +1,4 @@
 <?php 
-// verify
-// caracter
-
-
 function vNumber($n) // is a number/ eh um numero ? / return true or false
 {
 // is a number ? return true or false // indentify strings 
@@ -12,8 +8,6 @@ elseif(!is_nan($n))
 {return true;}
 }
 
-
-
 // database
 function connectDataBase($nome_host,$nome_db,$user,$senha)
 {
@@ -21,26 +15,45 @@ function connectDataBase($nome_host,$nome_db,$user,$senha)
     return $conn;
 }
 //crud
-function selectAll_obj($conn, $table)
-{   // return * values of your table in fetchALL in obj format"
+function selectAll($conn, $table, $typeReturn)
+{    // $typeReturn -> return OBJ or ARRAY, 1 or 2 
+     // return * values of your table in fetchALL in obj format"
+    if($typeReturn != 1 && $typeReturn != 2){  die("<b>ERROR FUNCTION</b> the typeReturn is diferent of 1 or 2, Select a Type"); }
+    else{ 
     $sql = "SELECT * FROM $table";
     $query = $conn->prepare($sql);
     $query->execute();
-    return $query->fetchALL(\PDO::FETCH_OBJ);
+    $ty = ($typeReturn == 1) ? $query->fetchALL(\PDO::FETCH_OBJ) : $query->fetchALL(\PDO::FETCH_ASSOC) ;
+    return $ty;
+    }
 }
-
-function selectAll_array($conn, $table)
-{   // return * values of your table in fetchALL in array format"
-    $sql = "SELECT * FROM $table";
-    $query = $conn->prepare($sql);
-    $query->execute();
-    return $query->fetchALL(\PDO::FETCH_ASSOC);
+function selectOnceEqual($conn,$param, $table, $column, $value, $typeReturn){
+    // $typeReturn -> return OBJ or ARRAY, 1 or 2 
+     // return one line values of your table in fetchALL in obj format"
+     if($typeReturn != 1 && $typeReturn != 2){  die("<b>ERROR FUNCTION</b> the typeReturn is diferent of 1 or 2, Select a Type"); }
+     else{ 
+     $sql = "SELECT $param FROM $table WHERE $column = :v";
+     $query = $conn->prepare($sql);
+     $query->bindValue(":v",$value);
+     $query->execute();
+     $ty = ($typeReturn == 1) ? $query->fetch(\PDO::FETCH_OBJ) : $query->fetchALL(\PDO::FETCH_ASSOC) ;
+     return $ty;
+    }
 }
-function select(){
-    $sql = "";
+function selectOnceDiferent($conn,$param, $table, $column, $value, $typeReturn){
+    // $typeReturn -> return OBJ or ARRAY, 1 or 2 
+     // return one line values of your table in fetchALL in obj format"
+     if($typeReturn != 1 && $typeReturn != 2){  die("<b>ERROR FUNCTION</b> the typeReturn is diferent of 1 or 2, Select a Type"); }
+     else{ 
+     $sql = "SELECT $param FROM $table WHERE $column != :v";
+     $query = $conn->prepare($sql);
+     $query->bindValue(":v",$value);
+     $query->execute();
+     $ty = ($typeReturn == 1) ? $query->fetch(\PDO::FETCH_OBJ) : $query->fetchALL(\PDO::FETCH_ASSOC) ;
+     return $ty;
+    }
 }
-
-function verifyInstance($conn,$param, $table, $columns, $values){
+function verifyInstanceDefault($conn,$param, $table, $columns, $values){
     // $conn -> conection ; $param -> the lines where you want return ;
     // $conn -> conexao com o banco de dados; $param -> as linhas(tuplas) que voce deseja retornar como verificacao do banco de dados, aconselhavel deixar como *(all ou tudo);
     // verify if the value are in the columns inserted in the array $columns;
@@ -55,6 +68,10 @@ function verifyInstance($conn,$param, $table, $columns, $values){
         case 'Tudo':
             $param = "*";
         break;
+
+        case 'TUDO':
+            $param = "*";
+        break;
         
         case 'all':
             $param = "*";
@@ -63,7 +80,12 @@ function verifyInstance($conn,$param, $table, $columns, $values){
         case 'All':
             $param = "*";
         break;
+        
+        case 'ALL':
+            $param = "*";
+        break;
     }
+
     $sql = "SELECT $param FROM $table WHERE ";
     $cnt = count($values);
     $i = 0;
@@ -84,16 +106,173 @@ function verifyInstance($conn,$param, $table, $columns, $values){
         return true;
     }
 }
+function verifyInstanceEqual_Or($conn,$param, $table, $columns, $values){
+    // $conn -> conection ; $param -> the lines where you want return ;
+    // $conn -> conexao com o banco de dados; $param -> as linhas(tuplas) que voce deseja retornar como verificacao do banco de dados, aconselhavel deixar como *(all ou tudo);
+    // verify if the value is diferent in the columns inserted in the array $columns;
+    // verifica se os valores estao nas colunas inseridas, respectivamente   
+    // $columns and $values need to in array format
+    // $columns e $values precisam estar no formato de array
+    switch ($param) {   
+        case 'tudo':
+            $param = "*";
+            break;
+        
+        case 'Tudo':
+            $param = "*";
+        break;
+
+        case 'TUDO':
+            $param = "*";
+        break;
+        
+        case 'all':
+            $param = "*";
+        break;
+
+        case 'All':
+            $param = "*";
+        break;
+        
+        case 'ALL':
+            $param = "*";
+        break;
+    }
+    
+    $sql = "SELECT $param FROM $table WHERE ";
+    $cnt = count($values);
+    $i = 0;
+    while ($i < $cnt) {
+        $vl_cout = "\"$values[$i]\"";
+        if($i == ($cnt-1)){$sql = $sql."$columns[$i] = "." $vl_cout "; $i = $cnt;}
+        if($i < $cnt){
+        $sql = $sql."$columns[$i] != "." $vl_cout AND ";
+        }
+        $i++;
+    }
+    $qry = $conn->prepare($sql);
+    $qry->execute();
+    $tmp3 = $qry->fetch(\PDO::FETCH_OBJ);
+    if($tmp3 == null){
+        return false;
+    }else{
+        return true;
+    }
+}
+function verifyInstanceDiferent($conn,$param, $table, $columns, $values){
+    // $conn -> conection ; $param -> the lines where you want return ;
+    // $conn -> conexao com o banco de dados; $param -> as linhas(tuplas) que voce deseja retornar como verificacao do banco de dados, aconselhavel deixar como *(all ou tudo);
+    // verify if the value is diferent in the columns inserted in the array $columns;
+    // verifica se os valores estao nas colunas inseridas, respectivamente   
+    // $columns and $values need to in array format
+    // $columns e $values precisam estar no formato de array
+    switch ($param) {   
+        case 'tudo':
+            $param = "*";
+            break;
+        
+        case 'Tudo':
+            $param = "*";
+        break;
+
+        case 'TUDO':
+            $param = "*";
+        break;
+        
+        case 'all':
+            $param = "*";
+        break;
+
+        case 'All':
+            $param = "*";
+        break;
+        
+        case 'ALL':
+            $param = "*";
+        break;
+    }
+    
+    $sql = "SELECT $param FROM $table WHERE ";
+    $cnt = count($values);
+    $i = 0;
+    while ($i < $cnt) {
+        $vl_cout = "\"$values[$i]\"";
+        if($i == ($cnt-1)){$sql = $sql."$columns[$i] = "." $vl_cout "; $i = $cnt;}
+        if($i < $cnt){
+        $sql = $sql."$columns[$i] != "." $vl_cout AND ";
+        }
+        $i++;
+    }
+    $qry = $conn->prepare($sql);
+    $qry->execute();
+    $tmp3 = $qry->fetch(\PDO::FETCH_OBJ);
+    if($tmp3 == null){
+        return false;
+    }else{
+        return true;
+    }
+}
+function verifyInstanceDiferent_Or($conn,$param, $table, $columns, $values){
+    // $conn -> conection ; $param -> the lines where you want return ;
+    // $conn -> conexao com o banco de dados; $param -> as linhas(tuplas) que voce deseja retornar como verificacao do banco de dados, aconselhavel deixar como *(all ou tudo);
+    // verify if the value is diferent in the columns inserted in the array $columns;
+    // verifica se os valores estao nas colunas inseridas, respectivamente   
+    // $columns and $values need to in array format
+    // $columns e $values precisam estar no formato de array
+    switch ($param) {   
+        case 'tudo':
+            $param = "*";
+            break;
+        
+        case 'Tudo':
+            $param = "*";
+        break;
+
+        case 'TUDO':
+            $param = "*";
+        break;
+        
+        case 'all':
+            $param = "*";
+        break;
+
+        case 'All':
+            $param = "*";
+        break;
+        
+        case 'ALL':
+            $param = "*";
+        break;
+    }
+    
+    $sql = "SELECT $param FROM $table WHERE ";
+    $cnt = count($values);
+    $i = 0;
+    while ($i < $cnt) {
+        $vl_cout = "\"$values[$i]\"";
+        if($i == ($cnt-1)){$sql = $sql."$columns[$i] = "." $vl_cout "; $i = $cnt;}
+        if($i < $cnt){
+        $sql = $sql."$columns[$i] != "." $vl_cout OR ";
+        }
+        $i++;
+    }
+    $qry = $conn->prepare($sql);
+    $qry->execute();
+    $tmp3 = $qry->fetch(\PDO::FETCH_OBJ);
+    if($tmp3 == null){
+        return false;
+    }else{
+        return true;
+    }
+}
 // inserction 
 function insertDefault($cn, $tb, $clmn,$values)
 {   // erros codes in the last line of the function
     //$cn -> connection database ;$tb -> table; $clmn -> columnns 
-    if(verifyInstance($cn, "*", $tb,$clmn,$values)!=true)
+    if(verifyInstanceDefault($cn, "*", $tb,$clmn,$values)!=false)
     {
-        return " ERROR 1 ";
+        die("ERROR 1");
     }
-    
-    // PAREI AJEITANDO O ARRAY DAS COLUNAS
     $tmp1 = "";
     $t = 0;
     $t1 = count($clmn);
@@ -126,6 +305,90 @@ function insertDefault($cn, $tb, $clmn,$values)
         return $cn->lastInsertId();
         // ERROR 1 ; THE VALUES EXISTS IN THE DATABASE !
         // IF YOU WANT THE VALUES REPEAT; USE THE FUNCTION insertDefault_repeat();
+    }
+    function insertDefault_repeat($cn, $tb, $clmn,$values)
+{   // erros codes in the last line of the function
+    // $cn -> connection database ;$tb -> table; $clmn -> columnns  
+   
+    $tmp1 = "";
+    $t = 0;
+    $t1 = count($clmn);
+    foreach ($clmn as $ac) 
+    {
+    $t++;
+    $tmp1 = $tmp1.$ac.",";
+    }
+    $tmp1 = substr($tmp1, 0, -1);
+        $cnt = count($values);
+        $tmp0 = "";
+        $algo = "";
+        for ($i=0; $i <= count($values); $i++) {
+            $tmp0 = $algo.$tmp0;
+            $algo = $tmp0;
+            $tmp0 = ":v$i,";
+        }
+        
+        //echo $algo;
+        $algo = substr($algo, 0, -1);        
+        $sql = "INSERT INTO $tb($tmp1) VALUES ($algo)";
+        $qry = $cn->prepare($sql);
+        $a = -1;
+        foreach($values as $v)
+        {   
+            $a++;
+            $qry->bindValue(":v$a", $v);
+        }
+        $qry->execute();
+        return $cn->lastInsertId();
+    }
+    function updateAll($conn, $table, $clmn, $vle){
+        // not recomend
+        $sql = "UPDATE $table SET $clmn = :v";
+        $qry = $conn->prepare($sql);
+        $qry->bindValue(":v",$vle);
+        $qry->execute();
+        return true;
+    }
+    function updateOnceById($conn, $table, $clmn, $vle, $column_id, $id){
+        $sql = "UPDATE $table SET $clmn = :v WHERE $column_id = $id";
+        $qry = $conn->prepare($sql);
+        $qry->bindValue(":v",$vle);
+        $qry->execute();
+        return true;
+    }
+    function updateById($conn, $table, $clmn, $vle,$column_id, $id){
+    // $conn -> conexao com o Banco de Dados / Conection with the DataBase
+    // $clmn -> PRECISA ESTAR EM FORMATO DE ARRAY | NEED TO STAY IN ARRAY FORMAT Colunas que voce deseja alterar //columns who you wish to alter OBS -> Devem ser ordenados respectivamente de acordo com a ordem dos Valores // They must be ordered respectively according to the order of Values
+    // $vle -> PRECISA ESTAR EM FORMATO DE ARRAY | NEED TO STAY IN ARRAY FORMAT Valores a setar // values to set // OBS -> Devem ser ordenados respectivamente de acordo com a ordem das colunas // They must be sorted respectively according to the order of the columns
+    // $column_id -> nome da coluna id da tabela desejada, ele quem diferencia quem vai sofrer o update || column name id of the desired table, he is the one who differentiates who will undergo the update
+    // $id -> valor do id; 
+       $sql = "UPDATE $table SET ";
+        $cnt = count($vle);
+        $i = 0;
+        while ($i < $cnt) {
+            $vl_cout = "\"$vle[$i]\"";
+            if($i == ($cnt-1)){$sql = $sql."$clmn[$i] = "." $vl_cout "; $i = $cnt;}
+            if($i < $cnt){
+            $sql = $sql."$clmn[$i] = "." $vl_cout, ";
+            }
+            $i++;
+        }
+        $sql = $sql." WHERE $column_id = $id";
+        $qry = $conn->prepare($sql);
+        $qry->execute();
+        //$tmp3 = $qry->fetch(\PDO::FETCH_OBJ);
+    }
+    function deleteAll($conn, $table){
+        //use apenas se voce precisar apagar a tabela inteira || just use if you want delete all data from table
+        $sql = "DELETE FROM $table";
+        $query = $conn->prepare($sql);
+        $query->execute();
+    }
+    function deleteById($conn, $table, $column_id, $id){
+        $sql = "DELETE FROM $table WHERE $column_id = :id";
+        $query = $conn->prepare($sql);
+        $query->bindValue("id",$id);
+        $query->execute();
     }
     //login
     function vLogin($cn,$tbl,$camp, $lg, $pswd)
@@ -205,7 +468,6 @@ function insertDefault($cn, $tb, $clmn,$values)
     // logoff 
     function logOff($pagina_retorno)
     {
-        
         if(empty($_SESSION))
         {
             vSession_start();
--- phpfuncs/index.php
+++ phpfuncs/index.php
@@ -4,11 +4,13 @@ include("functions.php");
 $conn = connectDataBase(HOST,DATABASE,$user,$senha);
 $get = (isset($_GET["add"])) ? isset($_GET["add"]) : null ; 
 $arr = array("nome","qtd","info");
-$values = array("Testekk2",10,"kakkk");
+$values = array("Ayron Lindo",90,"RESPEITE O HOMI");
 if($get == 1){
     //insertDefault($conn,"produtos",$arr,$values);
-   if(verifyInstance($conn,"all","produtos",$arr,$values)){echo "salve";}else{echo "Deu ruim";};
-} 
+    //updateById($conn,"produtos", $arr, $values,"id_produto",1);
+    //deleteById($conn,"produtos","id_produto",1);
+   print_r(selectOnceDiferent($conn, "nome","usuario","nome", "ayron", 1));
+}
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
